7268183e0a7e43fe16c1ec3bc78da79fad93aa92
fenjianwei <fengjw@ktc.cn>
2018-12-12 17:02:31 +0800

    heart canvas method code commit.

---
+++ app/src/main/java/com/fengjw/customviewdemo/view/HeartView.java
@@ -0,0 +1,19 @@
+package com.fengjw.customviewdemo.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class HeartView extends View {
+
+    private
+
+    public HeartView(Context context) {
+        super(context);
+    }
+
+    public HeartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
